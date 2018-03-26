ff875c90ff6885e5e63550c6fb57e38f1480dfa8
Ahmed-Anwer <ahmed.anwer.ezzeldien@gmail.com>
2018-03-26 14:45:33 +0300

    Initial commit

---
+++ app/src/main/java/com/ahmedelbossily/soundrecorder/activites/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ahmedelbossily.soundrecorder.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ahmedelbossily.soundrecorder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/ahmedelbossily/soundrecorder/fragments/RecordFragment.java
@@ -0,0 +1,10 @@
+package com.ahmedelbossily.soundrecorder.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by AhmedElbossily on 26/03/2018.
+ */
+
+public class RecordFragment extends Fragment {
+}
